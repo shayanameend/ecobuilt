1b3c085141ddfdced40ad555e7a8770c2ff3987b
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-27 13:53:54 +0500

    feat: establish database connection in server initialization

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import { createServer as createHttpServer } from "node:http";
 
 import { app } from "@/app";
 import { env } from "@/lib/env";
+import { connectDB } from "@/lib/db";
 
 const server = createHttpServer(app);
+
+connectDB();
 
 server.listen({ port: env.PORT }, () => {
   console.log(`Server is live on http://localhost:${env.PORT}`);
