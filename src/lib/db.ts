561c227797ab34a93d1348b060eaf9fbbd3ed138
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-27 13:51:59 +0500

    refactor: simplify MongoDB connection logic in db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,12 +3,7 @@ import mongoose from "mongoose";
 import { env } from "@/lib/env";
 
 export const connectDB = () => {
-  mongoose
-    .connect(env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((data) => {
-      console.log(`MongoDB Connected with Server: ${data.connection.host}`);
-    });
+  mongoose.connect(env.DB_URL).then((data) => {
+    console.log(`MongoDB Connected with Server: ${data.connection.host}`);
+  });
 };
